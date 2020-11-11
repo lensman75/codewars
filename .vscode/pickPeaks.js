e12251cc3091d68ad3c1e39966bc07ad0267e746
Lensman75 <anministry@gmail.com>
2020-11-11 18:02:27 +0200

    Unfinished pickPeaks.

--- .vscode/pickPeaks.js
+++ .vscode/pickPeaks.js
@@ -20,29 +20,57 @@ function pickPeaks(arr) {
     let answer = {}
     let peak = []
     let pos = []
+    // if (arr[0] > arr[1]) {
+    //     peak.push(arr[0]);
+    //     pos.push
+    // }
     if (arr.length == 0) {
-        return answer.pos = [], answer.peaks = [];
+        answer["pos"] = pos;
+        answer["peaks"] = peak;
+        return answer;
     } else {
-        if (arr[0] > arr[1]) {
-            peak.push(arr[0]);
-            pos.push(indexOf(arr[0]));
-        } else {
-            for (let i = 1; i < arr.length; i++) {
-                if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1] )){
+        for (let i = 1; i < arr.length; i++) {
+            if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1])) {
+                peak.push(arr[i]);
+                pos.push(i);
+            } else {
+                if (arr[i] == arr[i + 1] && arr[i] > arr[i - 1]) {
                     peak.push(arr[i]);
-                    pos.push(arr.indexOf(arr[i]));
-                    console.log(arr.indexOf(6))
+                    pos.push(i);
                 }
+                if ((arr[i] == arr[i - 1] && arr[i] == arr[i + 1]) || (arr[i] == arr[i + 1] && arr[i] < arr[i - 1]) || (arr[i] == arr[i-1] && arr[i] < arr[i+1])) {
+                    continue;
+                }
+                // if ((arr[i] == arr[i - 1] && arr[i] < arr[i + 1]) || (arr[i] == arr[i - 1] && i == arr.length - 1)) {
+                //     peak.pop();
+                //     pos.pop();
+                // }
             }
         }
-
     }
-    answer["peak"] = peak;
-    answer["pos"] = pos;
 
+    answer["pos"] = pos;
+    answer["peaks"] = peak;
     return answer;
 }
 
-console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]))
-
-// 1, 2, 3, 6, 4, 1, 2, 3, 2, 1
+// console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]));
+//                                                         //{pos:[3,7], peaks:[6,3]});
+// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]));
+//                                                         // {pos:[3,7], peaks:[6,3]});
+// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]))
+//                                                         // {pos:[3,7,10], peaks:[6,3,2]});
+// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]))
+//                                                         // {pos:[2,4], peaks:[3,2]});
+// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]))
+//                                                         // {pos:[2], peaks:[3]});
+// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]))
+//                                                         // {pos:[2], peaks:[3]});
+// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]))
+//                                                         // {pos:[2], peaks:[3]});
+// console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]))
+//                                                         // {pos:[2,7,14,20], peaks:[5,6,5,5]});
+// console.log(pickPeaks([]))
+//                                                         // {pos:[],peaks:[]});
+// console.log(pickPeaks([1, 1, 1, 1]));
+//                                                         //{pos:[],peaks:[]});
