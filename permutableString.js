e9450f1c65721c2418f779bd11da9fe0db2665a3
Lensman75 <anministry@gmail.com>
2020-11-25 17:43:08 +0200

    Refactored in function.

--- permutableString.js
+++ permutableString.js
@@ -3,20 +3,21 @@
 //Works when both of strings are the same length.
 function permutable(s1, s2) {
     let obj1 = {}, obj2 = {};
-    for (let i = 0; i < s1.length; i++) {
-        if (!(s1[i] in obj1)) {
-            obj1[s1[i]] = 1;
-        } else {
-            obj1[s1[i]] += 1;
+
+    function countSymbols(str, obj) {
+        for (let i = 0; i < str.length; i++) {
+            if (!(str[i] in obj)) {
+                obj[str[i]] = 1;
+            } else {
+                obj[str[i]] += 1;
+            }
         }
+        return obj;
     }
-    for (let a = 0; a < s2.length; a++) {
-        if (!(s2[a] in obj2)) {
-            obj2[s2[a]] = 1;
-        } else {
-            obj2[s2[a]] += 1;
-        }
-    }
+    
+    countSymbols(s1, obj1);
+    countSymbols(s2, obj2);
+
 
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
