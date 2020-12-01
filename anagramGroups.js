6304e49b747aa6ff439e02fa5afa01ea8d05f251
Lensman75 <anministry@gmail.com>
2020-12-02 00:43:14 +0200

    Fixed comparison of two objects. Seems it works.

--- anagramGroups.js
+++ anagramGroups.js
@@ -12,7 +12,6 @@ function compareObjects(item1, item2) {
       obj1[(item1[i] = obj1[item[i]] + 1)];
     }
   }
-  //   obj1;
   for (let i = 0; i < item2.length; i++) {
     if (!(item2[i] in obj2)) {
       obj2[item2[i]] = 1;
@@ -20,17 +19,16 @@ function compareObjects(item1, item2) {
       obj2[(item2[i] = obj2[item[i]] + 1)];
     }
   }
-  //   obj2;
 
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+  const keys1 = Object.keys(item1);
+  const keys2 = Object.keys(item2);
 
-  if (keys1.length !== keys2.length) {
+  if (!(keys1.length == keys2.length)) {
     return false;
   }
 
   for (let keys in keys1) {
-    if (!(keys1[keys] !== keys2[keys])) {
+    if (!(keys1[keys] == keys2[keys])) {
       return false;
     }
   }
@@ -48,13 +46,13 @@ for (let i = 1; i < strs.length; i++) {
   } else {
     continue;
   }
-  for (let x = strs.length; x > 0; x--) {
-    if (compareObjects(strs[0], strs[x - 1]) == true){
-      strs.splice(x-1,1);
+  //   for (let x = strs.length; x > 0; x--) {
+  //     if (compareObjects(strs[0], strs[x - 1]) == true){
+  //       strs.splice(x-1,1);
 
-    } else {continue}
-    strs.shift();
-  }
+  //     } else {continue}
+  //     strs.shift();
+  //   }
   counter += 1;
 }
 
